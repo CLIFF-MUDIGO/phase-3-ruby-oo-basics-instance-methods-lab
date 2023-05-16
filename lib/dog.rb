48e8a445c88b336c2231cf3e32e2affc440cc9eb
cliff <cliffmudigo5@gmail.com>
2023-05-16 21:09:02 +0300

    completed assignment

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+# dog.rb
+
+class Dog
+    def bark
+      puts "Woof!"
+    end
+  
+    def sit
+      puts "The Dog is sitting"
+    end
+  end
+  
+  # person.rb
+
+class Person
+    def talk
+      puts "Hello World!"
+    end
+  
+    def walk
+      puts "The Person is walking"
+    end
+  end
+  
